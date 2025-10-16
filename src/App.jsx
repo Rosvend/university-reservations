aafb853d963585258d399bb1301872bf03fd9cc8
rosvend <roy.sandoval@upb.edu.co>
2025-10-16 12:13:19 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Sistema de Reservas Universitarias</h1>
+      <p>¡Proyecto funcionando!</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/tests/setup.js
@@ -0,0 +1,19 @@
+import '@testing-library/jest-dom'
+```
+
+**9. `.gitignore`** (if you don't have one)
+```
+node_modules
+
+dist
+
+.env
+.env.local
+
+.vscode
+.idea
+
+.DS_Store
+Thumbs.db
+
+*log
